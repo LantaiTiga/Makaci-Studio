b9e5d6b239b22eac9d33842cc5cab2f43cb53048
Alan-Alkalifa <work.alanalkalifa@gmail.com>
2024-10-30 17:46:15 +0700

    Update

--- app/component/ui/navbar.js
+++ app/component/ui/navbar.js
@@ -13,20 +13,19 @@ export function Navbar() {
       catalog-product
       </Link>
       <div className='flex flex-row justify-between'>
-      <Link className={`link ${pathname === '/potery-class' ? 'active' : ''}`} href="/potery-class">
+      <Link className={`link ${pathname === '/potery-class' ? 'active' : 'border-2 border-b-black'}`} href="/potery-class">
         catalog-product
       </Link>
 
-      <Link className={`link ${pathname === '/costum-order' ? 'active' : ''}`} href="/costum-order">
+      <Link className={`link ${pathname === '/costum-order' ? 'active' : 'border-2 border-b-black'}`} href="/costum-order">
         catalog-product
       </Link>
 
-      <Link className={`link ${pathname === '/catalog-product' ? 'active' : ''}`} href="/catalog-product">
+      <Link className={`link ${pathname === '/catalog-product' ? 'active' : 'border-2 border-b-black'}`} href="/catalog-product">
         catalog-product
       </Link>
  
-      <Link
-        className={`link ${pathname === '/faq' ? 'active' : ''}`} href="/faq">
+      <Link className={`link ${pathname === '/faq' ? 'active' : 'border-2 border-b-black'}`} href="/faq">
         About
       </Link>
       </div>
